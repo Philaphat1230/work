0fc56da474c84aeb58ea6e86181de366873a8278
Philaphat1230 <3651051541175@rmutr.ac.th>
2024-09-18 10:11:50 +0700

    Add files via upload

---
+++ bank.dart
@@ -0,0 +1,50 @@
+class BankAccount {
+  String? _name;
+  double? _balance;
+  
+  BankAccount(this._name,this._balance);
+
+  double get balance => this._balance!;
+  set balance(double balance) => _balance = balance;
+
+  void deposit(double amount){
+    this._balance = balance + amount; 
+    print('ชื่อบัญชี : $_name');
+    print('ฝาก $amount');
+    print('จำนวนเงินคงเหลือ $_balance');
+  }
+  void withdraw(double amount){
+    this._balance = balance - amount;
+    print('ชื่อบัญชี : $_name');
+    print('ถอน $amount');
+    print('จำนวนเงินคงเหลือ $_balance');
+  }
+  void showBalance(){
+    print('ชื่อบัญชี $_name');
+    print('จำนวนเงินคงเหลือ $_balance');
+  }
+}
+
+class SavingAccount extends BankAccount {
+  double _interestRate;
+  SavingAccount(_name,_balance,this._interestRate):super(_name,_balance);
+  void addlnterest([double? newinterestRate]){
+    double x = balance * this._interestRate /100;
+    balance += x;
+    print('ชื่อบัญชี : $_name');
+    print('ดอกเบี้ย : $_interestRate เปอร์เซนต์');
+    print('ได้รับดอกเบี้ย : $x บาท');
+    print('จำนวนเงินคงเหลือ $_balance');
+  }
+
+ 
+}
+
+void main(List<String> args) {
+  var s = SavingAccount('xxxxxxxxxxx', 10000.00, 0.50);
+  s.showBalance();
+  s.deposit(500);
+  s.withdraw(200);
+  s.addlnterest();
+  s.showBalance();
+}
